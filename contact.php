3e3a8765c21fee3bd2c284398d90a9627730c426
Pawan singh <52067715+pawan-web@users.noreply.github.com>
2021-07-31 12:31:37 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php
+//get data from form  
+
+$name = $_POST['Your name'];
+$email= $_POST['Your email'];
+$message= $_POST['Your message'];
+$to = "20pawansingh@mail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:index.html");
+?>
